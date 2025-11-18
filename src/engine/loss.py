82da6774ba213ff2f9e348506b8eb22975163260
Álvaro Palacios <polliferoselretorno2019@gmail.com>
2025-11-18 17:12:40 +0100

    Loss definida y correciones en los pesos

--- src/engine/loss.py
+++ src/engine/loss.py
@@ -1,3 +1,28 @@
 import torch
+import numpy as np
+import torch.nn as nn
+from piqa import MS_SSIM
+
+def loss_radar(pred, target, a, b):
+    bins = torch.tensor([-10.,0.,10.,20.,30.,40.,50.,80.], device = target.device, dtype=target.dtype)
+    bins_normalizados = (bins + 10) / 90
+
+    pesos = torch.tensor([0.01,   # [-10,  0) no lluvia / ruido
+                          0.08,   # [  0, 10) lluvia muy ligera
+                          0.80,   # [ 10, 20) lluvia típica suave   
+                          1.00,   # [ 20, 30) lluvia moderada       
+                          3.00,   # [ 30, 40) lluvia fuerte
+                          12.0,   # [ 40, 50) tormenta fuerte
+                          70.0    # [ 50, 80) núcleos / tormentas muy fuertes
+                          ], device=target.device, dtype=target.dtype)
+    
+    loss_mse = nn.MSELoss(reduction = "none")
+    ms_ssim = MS_SSIM(data_range = 1.0)
+    matriz_pesos = pesos[torch.bucketize(target, boundaries= bins_normalizados) - 1]
+    score_mse = loss_mse(pred, target)
+    score_msssim = 1 - ms_ssim(pred, target)
+    loss_final = a*((matriz_pesos*score_mse).mean()) + b*score_msssim
+    return loss_final
+    
 
 
--- src/utils/shard_stats.py
+++ src/utils/shard_stats.py
@@ -8,7 +8,7 @@ np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format})
 
 
 def generar_freq_pixeles(shards_pattern="./data/raw/shards/*", 
-                       bins=np.array([-10,0,10,20,30,40,50,60,70,80]), 
+                       bins=np.array([-10,0,10,20,30,40,50,80]), 
                        output_path="./src/utils/freq_pixeles.npy"):
     
     acum = np.zeros(len(bins) - 1)
@@ -29,24 +29,23 @@ def generar_freq_pixeles(shards_pattern="./data/raw/shards/*",
 
 
 
-def generar_pesos_perdida(frecuencia_pixeles):
-    """
-    Genera los pesos para la función de pérdida del modelo
-    con el fin de compensar el fuerte desbalance en la distribución
-    de valores de reflectividad (dBZ) en los datos.
-    """
-    pesos_inv = 1/np.array(frecuencia_pixeles)**0.2
-    pesos_norm = pesos_inv/np.mean(pesos_inv)
-    pesos_one = np.ones_like(pesos_norm)
-    pesos_one[0] *= 0.05
-    pesos_one[1] *= 1.25
-    pesos_one[2] *= 1.8
-    pesos_one[3] *= 1.5
-    pesos_one[5] *= 2
-    pesos_one[6] *= 2
+# def generar_pesos_perdida(frecuencia_pixeles):
+#     """
+#     Genera los pesos para la función de pérdida del modelo
+#     con el fin de compensar el fuerte desbalance en la distribución
+#     de valores de reflectividad (dBZ) en los datos.
+#     """
+#     pesos_inv = 1/np.array(frecuencia_pixeles)**0.2
+#     pesos_norm = pesos_inv/np.mean(pesos_inv)
+#     pesos_one = np.ones_like(pesos_norm)
+#     pesos_one[0] *= 0.05
+#     pesos_one[1] *= 1.25
+#     pesos_one[2] *= 1.8
+#     pesos_one[3] *= 1.5
+#     pesos_one[5] *= 2
+#     pesos_one[6] *= 2
 
-    nuevos_pesos = pesos_norm*pesos_one
-    nuevos_pesos = nuevos_pesos / np.mean(nuevos_pesos)
-    return nuevos_pesos
-
+#     nuevos_pesos = pesos_norm*pesos_one
+#     nuevos_pesos = nuevos_pesos / np.mean(nuevos_pesos)
+#     return nuevos_pesos
 
